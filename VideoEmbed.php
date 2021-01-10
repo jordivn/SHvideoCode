32f05b11d3e0edea59d35ba65e42e2e5bc65bc0e
Jordi van Nistelrooij @ Webs en Systems <info@websensystems.nl>
2021-01-10 20:48:15 +0100

    Videodownload preventions

--- VideoEmbed.php
+++ VideoEmbed.php
@@ -22,8 +22,8 @@
  ?>
  <body style=background-color:black;>
 
-<center>
-<video style='width:95vw;max-height:95vh' controls autoplay>
+ <center>
+<video oncontextmenu="return false;" style='width:95vw;max-height:95vh' controlsList="nodownload" controls autoplay>
   <source src="films/movie.mp4" type="video/mp4">
   <source src="films/movie.ogg" type="video/ogg">
 Your browser does not support the video tag.
